f4bd47738e835ac81fd8588d5c60bef4d434cb7f
samwakelam <70378021+samwakelam@users.noreply.github.com>
2024-07-01 14:10:56 +0000

    feat: env variable update

--- apps/national-trust-tracker-app/library/helpers/getDatabaseConnection.helper.ts
+++ apps/national-trust-tracker-app/library/helpers/getDatabaseConnection.helper.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const getDatabaseConnection = async (): Promise<typeof mongoose> => {
-    const url = process.env.MONGO_URI as string;
+    const url = process.env.MONGODB_URI as string;
 
     return await mongoose.connect(url);
 };
