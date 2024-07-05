5b22666109e241f745e5a3383e1074e448e19f3b
samwakelam <70378021+samwakelam@users.noreply.github.com>
2024-07-05 13:30:56 +0000

    fix: build error data not on action response

--- apps/national-trust-tracker-app/app/Stats/page.tsx
+++ apps/national-trust-tracker-app/app/Stats/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 
 import { StatisticsView } from './partials';
 import { getMembership } from '../../actions/Membership.actions';
+import Loading from '../loading';
 
 export const metadata: Metadata = {
     title: 'National Trust Tracker',
@@ -10,5 +11,9 @@ export const metadata: Metadata = {
 export default async function Stats(): Promise<JSX.Element> {
     const membership = await getMembership('King');
 
-    return <StatisticsView membership={membership.data} />;
+    return membership.message === 'Success' ? (
+        <StatisticsView membership={membership.data} />
+    ) : (
+        <Loading />
+    );
 }
