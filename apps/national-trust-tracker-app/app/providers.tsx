c7e384b1bdc195a5b26fb5161e61406599dc520b
samwakelam <70378021+samwakelam@users.noreply.github.com>
2024-07-05 13:35:03 +0000

    fix: data does noty exist on action response

--- apps/national-trust-tracker-app/app/providers.tsx
+++ apps/national-trust-tracker-app/app/providers.tsx
@@ -11,8 +11,12 @@ export async function Providers({ children }: { children: ReactNode }) {
     const res = await Promise.all([await getAllVisits(), await getAllPlaces()]);
 
     return (
-        <VisitsProvider initial={res[0].data}>
-            <PlacesProvider initial={res[1].data}>
+        <VisitsProvider
+            initial={res[0].message === 'Success' ? res[0].data : []}
+        >
+            <PlacesProvider
+                initial={res[1].message === 'Success' ? res[1].data : []}
+            >
                 <ToastProvider>{children}</ToastProvider>
             </PlacesProvider>
         </VisitsProvider>
